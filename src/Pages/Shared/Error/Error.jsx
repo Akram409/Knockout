dfc2612f444bfb0fd5ddbfab3dfb442740dbefa7
RaW <akram.iiuc.ctg@gmail.com>
2023-06-13 23:39:49 +0600

    Loading styled

--- src/Pages/Shared/Error/Error.jsx
+++ src/Pages/Shared/Error/Error.jsx
@@ -5,11 +5,11 @@ import error from "../../../assets/404.json"
 const Error = () => {
     return (
         <div className="">
-         <div className="flex justify-center mt-5">
+         <div className="flex justify-center mt-10">
          <Link to="/"><button className="btn btn-primary font-bold text-white">Back to Home</button></Link>
          </div>
          <div className="relative lg:w-[100vh] mx-auto ">
-          <div className="w-[100vh]  sm:h-96">
+          <div className="w-[90vh]  sm:h-96">
             <Lottie animationData={error} loop={true} />
           </div>
         </div>
--- src/Pages/Shared/Loading/Loading.jsx
+++ src/Pages/Shared/Loading/Loading.jsx
@@ -1,13 +1,14 @@
 import Lottie from "lottie-react";
-import loading from "../../../assets/loading.json"
-
+import loading from "../../../assets/loading.json";
 
 const Loading = () => {
-    return (
-        <div className="">
-            <Lottie animationData={loading} loop={true} />
-       </div>
-    );
+  return (
+    <div className="relative lg:w-[100vh] mx-auto ">
+      <div className="w-[90vh]  sm:h-96">
+        <Lottie animationData={loading} loop={true} />
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
